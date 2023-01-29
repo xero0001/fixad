59f5d5e0224faeef60244bb78c518d7c32bf9513
jejunpark <jejunpark1@gmail.com>
2023-01-29 14:12:03 +0900

    nav bar develop start

--- src/app/(service)/layout.tsx
+++ src/app/(service)/layout.tsx
@@ -1,12 +1,14 @@
 import styles from './layout.module.css'
 import classNames from 'classnames/bind'
+import NavBar from './(_components)/navBar/NavBar'
 
 const cx = classNames.bind(styles)
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={cx('container')}>
-      <main>{children}</main>
-    </div>
+    <>
+      <NavBar />
+      <main className={cx('container')}>{children}</main>
+    </>
   )
 }
