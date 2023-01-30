40b03e1cb1774690f7b48b23df75b02e1fe7a193
jejunpark <jejunpark1@gmail.com>
2023-01-31 00:59:28 +0900

    result modal add

--- src/app/(service)/auth/layout.tsx
+++ src/app/(service)/auth/layout.tsx
@@ -1,6 +1,8 @@
 import styles from './layout.module.css'
 import classNames from 'classnames/bind'
 import Image from 'next/image'
+import Link from 'next/link'
+import { PATH } from '@root/src/shared/const'
 
 const cx = classNames.bind(styles)
 
@@ -8,7 +10,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={cx('auth-page-layout')}>
       <h1 className={cx('logo-wrapper')}>
-        <Image src="/assets/logo/logo.png" alt="teacher-cloud" width={94} height={32} />
+        <Link href={PATH.HOME}>
+          <Image src="/assets/logo/logo.png" alt="teacher-cloud" width={94} height={32} />
+        </Link>
       </h1>
       {children}
     </div>
