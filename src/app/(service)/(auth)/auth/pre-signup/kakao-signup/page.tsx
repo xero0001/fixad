742f609539f8b516c207c5e24bc78b0e0e786f9b
ARAIS <xero0001@naver.com>
2023-01-31 22:30:31 +0900

    Fix

---
+++ src/app/(service)/(auth)/auth/pre-signup/kakao-signup/page.tsx
@@ -0,0 +1,8 @@
+// 'use client'
+
+// import { useAccount } from '@client/hooks'
+// import { PATHNAME } from '@root/src/client/consts'
+
+export default function Kakao() {
+  return <div>REDIRECTING...</div>
+}
