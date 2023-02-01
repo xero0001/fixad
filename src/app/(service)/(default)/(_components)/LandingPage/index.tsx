c07e27be8190fc41792b4218d547cb244749f447
jejunpark <jejunpark1@gmail.com>
2023-02-01 22:44:50 +0900

    스크롤 애니메이션 추가

--- src/app/(service)/(default)/(_components)/LandingPage/index.tsx
+++ src/app/(service)/(default)/(_components)/LandingPage/index.tsx
@@ -5,8 +5,9 @@ import IntroduceArea from '../IntroduceArea'
 import VisionArea from '../VisionArea'
 import ServiceArea from '../ServiceArea'
 import JoinArea from '../JoinArea'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Footer from '../../../(_components)/footer/Footer'
+import { BsChevronDown } from 'react-icons/bs'
 
 // import ReactFullpage from '@fullpage/react-fullpage'
 import ReactFullpage from '@fullpage/react-fullpage'
@@ -18,8 +19,14 @@ import styles from './LandingPage.module.css'
 const cx = classNames.bind(styles)
 
 export default function LandingPage() {
+  const [activeSectionNo, setActiveSectionNo] = useState(0)
+
   return (
     <>
+      <div className={cx('scroll-btn', { white: activeSectionNo === 1, disabled: activeSectionNo === 3 })}>
+        <span>SCROLL</span>
+        <BsChevronDown className={cx('scroll-icon')} />
+      </div>
       <ReactFullpage
         //fullpage options
         // licenseKey={''}
@@ -30,26 +37,30 @@ export default function LandingPage() {
           // @ts-ignore
           const currentIndex = activeSection?.index()
 
+          setActiveSectionNo(currentIndex)
+
           return (
-            <ReactFullpage.Wrapper>
-              <div className={cs('section', cx('fullpage', 's1'))}>
-                <IntroduceArea isActive={currentIndex === 0} />
-              </div>
-              <div className={cs('section', cx('fullpage', 's2'))}>
-                <VisionArea isActive={currentIndex === 1} />
-                <div className={cx('illust-wrapper', { active: currentIndex === 1 })}>
-                  <div className={cx('illust')}>
-                    <img src="/assets/illust/illust_127.svg" alt="illust_127" />
+            <>
+              <ReactFullpage.Wrapper>
+                <div className={cs('section', cx('fullpage', 's1'))}>
+                  <IntroduceArea isActive={currentIndex === 0} />
+                </div>
+                <div className={cs('section', cx('fullpage', 's2'))}>
+                  <VisionArea isActive={currentIndex === 1} />
+                  <div className={cx('illust-wrapper', { active: currentIndex === 1 })}>
+                    <div className={cx('illust')}>
+                      <img src="/assets/illust/illust_127.svg" alt="illust_127" />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className={cs('section', cx('fullpage', 's3'))}>
-                <ServiceArea isActive={currentIndex === 2} />
-              </div>
-              <div className={cs('section', cx('fullpage', 's4'))}>
-                <JoinArea isActive={currentIndex === 3} />
-              </div>
-            </ReactFullpage.Wrapper>
+                <div className={cs('section', cx('fullpage', 's3'))}>
+                  <ServiceArea isActive={currentIndex === 2} />
+                </div>
+                <div className={cs('section', cx('fullpage', 's4'))}>
+                  <JoinArea isActive={currentIndex === 3} />
+                </div>
+              </ReactFullpage.Wrapper>
+            </>
           )
         }}
       />
