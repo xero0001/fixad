4f5aa1cdc819e57fdc20274bc747136930fb8ba5
ARAIS <xero0001@naver.com>
2023-03-27 00:00:56 +0900

    Edit metadata

--- src/shared/const/metaData.ts
+++ src/shared/const/metaData.ts
@@ -6,11 +6,11 @@
 - 시스템 URL
 */
 export const METADATA = {
-  TITLE: '일잘러교사의 업무 메이트[티쳐클라우드]',
-  DESC: '선착순 2,000명 쿠폰제공. 매주 업로드되는 도안과 일지샘플',
-  IMAGE: 'https://teachercloud.s3.ap-northeast-2.amazonaws.com/OG/og_image.png',
-  IMAGE_RECTANGLE: 'https://teachercloud.s3.ap-northeast-2.amazonaws.com/OG/og_image.png',
-  URL: 'http://teachercloud.co/',
+  TITLE: '템플릿 프로젝트',
+  DESC: '템플릿으로 ROI 올리기 프로젝트',
+  IMAGE: '',
+  IMAGE_RECTANGLE: '',
+  URL: '',
   IMAGE_WIDTH: '1200',
   IMAGE_HEIGHT: '628',
 }
