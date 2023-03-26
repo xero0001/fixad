122620ff5d62fa02fa3efa057c859f4c13261825
ARAIS <xero0001@naver.com>
2023-03-27 01:22:54 +0900

    234

---
+++ src/app/(service)/(default)/(_components)/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className="border-t border-t-gray-200">ABC</div>
+}
--- src/app/(service)/(default)/layout.tsx
+++ src/app/(service)/(default)/layout.tsx
@@ -1,5 +1,5 @@
 import Header from '../(_components)/Header'
-import Footer from '../(_components)/Footer'
+import Footer from './(_components)/Footer'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
