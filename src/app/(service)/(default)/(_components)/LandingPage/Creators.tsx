05a43ff49b66403abfc619048d322569ab56e748
ARAIS <xero0001@naver.com>
2023-06-20 04:11:36 +0900

    Fix

--- src/app/(service)/(default)/(_components)/LandingPage/Creators.tsx
+++ src/app/(service)/(default)/(_components)/LandingPage/Creators.tsx
@@ -6,11 +6,14 @@ import { SlSocialInstagram, SlSocialYoutube } from 'react-icons/sl'
 import { SiLinktree } from 'react-icons/si'
 import { RxInstagramLogo } from 'react-icons/rx'
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+const creatorItems = [
+  {
+    name: '이진세',
+    href: 'https://www.instagram.com/jinselee_/?igshid=YmMyMTA2M2Y%3D',
+    title: 'MMA 파이터',
+    src: '/assets/profiles/이진세/ljs_1.png',
+  },
+  { name: '진다율', href: 'https://linktr.ee/jindayul', title: '모델', src: '/assets/profiles/진다율/1.jpg' },
 ]
 
 export default function Creators() {
@@ -21,68 +24,41 @@ export default function Creators() {
       <div className="text-4xl font-bold tracking-tight text-black sm:text-6xl text-center ">CREATORS</div>
       <div className="mx-auto max-w-7xl mt-16">
         <div className="grid grid-cols-2 gap-6 mx-auto">
-          <a className="relative" href="https://www.instagram.com/jinselee_/?igshid=YmMyMTA2M2Y%3D" target="_blank">
-            <div className="bg-gray-100 aspect-[3/4] w-full relative">
-              <div className="absolute w-full h-full">
-                <img src="/assets/profiles/이진세/ljs_1.png" alt="ljs" className="w-full h-full object-cover" />
-              </div>
-            </div>
-            <div
-              className="flex flex-col px-6 py-6 w-full space-y-2 lg:space-y-3 absolute text-white"
-              style={{
-                transform: 'translateY(-100%)',
-                backgroundColor: 'rgba(0,0,0,0.5)',
-              }}>
-              <div className="flex items-end text-3xl lg:text-5xl space-x-4 lg:space-x-6">
-                <div className="flex flex-col">
-                  <span>이진세</span>
+          {creatorItems.map((item, index) => {
+            return (
+              <a key={index} className="relative" href={item.href} target="_blank">
+                <div className="bg-gray-100 aspect-[3/4] w-full relative">
+                  <div className="absolute w-full h-full">
+                    <img src={item.src} alt="ljs" className="w-full h-full object-cover" />
+                  </div>
                 </div>
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-10 h-10 lg:w-[56px] lg:h-[56px]">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                  </svg>
-                </span>
-              </div>
-              <div className="text-lg lg:text-2xl">MMA 파이터</div>
-            </div>
-          </a>
-          <a className="relative" href="https://linktr.ee/jindayul" target="_blank">
-            <div className="bg-gray-100 aspect-[3/4] w-full relative">
-              <div className="absolute w-full h-full">
-                <img src="/assets/profiles/진다율/1.jpg" alt="ljs" className="w-full h-full object-cover" />
-              </div>
-            </div>
-            <div
-              className="flex flex-col px-6 py-6 w-full space-y-2 lg:space-y-3 absolute text-white"
-              style={{
-                transform: 'translateY(-100%)',
-                backgroundColor: 'rgba(0,0,0,0.5)',
-              }}>
-              <div className="flex items-end text-3xl lg:text-5xl space-x-4 lg:space-x-6">
-                <div className="flex flex-col">
-                  <span>진다율</span>
+                <div
+                  className="flex flex-col px-6 py-6 w-full space-y-2 lg:space-y-3 absolute text-white"
+                  style={{
+                    transform: 'translateY(-100%)',
+                    backgroundColor: 'rgba(0,0,0,0.5)',
+                  }}>
+                  <div className="flex items-end text-3xl lg:text-5xl space-x-4 lg:space-x-6">
+                    <div className="flex flex-col">
+                      <span>{item.name}</span>
+                    </div>
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-10 h-10 lg:w-[56px] lg:h-[56px]">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div className="text-lg lg:text-2xl">{item.title}</div>
                 </div>
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-10 h-10 lg:w-[56px] lg:h-[56px]">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                  </svg>
-                </span>
-              </div>
-              <div className="text-lg lg:text-2xl">모델</div>
-            </div>
-          </a>
+              </a>
+            )
+          })}
         </div>
       </div>
     </div>
