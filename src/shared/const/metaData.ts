595683141671debcf4f2d53ec4473691afdcca2f
ARAIS <xero0001@naver.com>
2023-06-21 01:00:23 +0900

    Fix

--- src/shared/const/metaData.ts
+++ src/shared/const/metaData.ts
@@ -8,8 +8,8 @@
 export const METADATA = {
   TITLE: 'BIG-G ENTERTAINMENT',
   DESC: '빅지 엔터테인먼트는 콘텐츠, 플랫폼을 통해 남성의 문화를 선도하는 기업입니다.',
-  IMAGE: '',
-  IMAGE_RECTANGLE: '',
+  IMAGE: '/thumbnail_rectangle.webp',
+  IMAGE_RECTANGLE: '/thumbnail_square.webp',
   URL: '',
   IMAGE_WIDTH: '1200',
   IMAGE_HEIGHT: '628',
