c115f80b83b75b62ee7ff98f571ea163fb6405a7
Henry <xero0001@naver.com>
2025-06-26 01:06:15 +0900

    test

--- src/app/(service)/(default)/(_components)/LandingPage/Hero.tsx
+++ src/app/(service)/(default)/(_components)/LandingPage/Hero.tsx
@@ -3,7 +3,7 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import EmblaCarousel from './EmblaCarousel'
 
-const OPTIONS = { loop: true }
+const OPTIONS = { loop: false }
 
 const SLIDES = [
   // {
