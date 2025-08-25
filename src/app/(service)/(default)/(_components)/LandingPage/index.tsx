371620ccf21a4c3a6e520910c1aec1a46917e059
xero0001 <xero0001@naver.com>
2025-08-26 03:01:06 +0900

    asdf

--- src/app/(service)/(default)/(_components)/LandingPage/index.tsx
+++ src/app/(service)/(default)/(_components)/LandingPage/index.tsx
@@ -7,6 +7,8 @@ import Contact from './Contact'
 import Services from './Services'
 // import { SectionsContainer, Section } from 'react-fullpage'
 
+// const FULLPAGE_LICENSE_KEY = '0L36I-3JVBI-OHZ87-IJR3H-QNIWN'
+
 export default function LandingPage({ lang = 'ko' }: { lang: string }) {
   const [activeSectionNo, setActiveSectionNo] = useState(0)
 
