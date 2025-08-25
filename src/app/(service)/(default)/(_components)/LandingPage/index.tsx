759b6373b5a15b0097d60c7030da8c554a70cb21
xero0001 <xero0001@naver.com>
2025-08-26 03:00:54 +0900

    asete

--- src/app/(service)/(default)/(_components)/LandingPage/index.tsx
+++ src/app/(service)/(default)/(_components)/LandingPage/index.tsx
@@ -1,20 +1,11 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Hero from './Hero'
-import Creators from './Creators'
 import CEO from './CEO'
-import Works from './Works'
-import Feature from './Feature'
-import SecondaryFeature from './SecondaryFeature'
 import Contact from './Contact'
 import Services from './Services'
-import Process from './Process'
-import FAQSection from './FAQSection'
-import Testimonial from './Testimonial'
 // import { SectionsContainer, Section } from 'react-fullpage'
-
-const FULLPAGE_LICENSE_KEY = '0L36I-3JVBI-OHZ87-IJR3H-QNIWN'
 
 export default function LandingPage({ lang = 'ko' }: { lang: string }) {
   const [activeSectionNo, setActiveSectionNo] = useState(0)
